caa61a0273fc30bfecdce280e2bcd1aa6e144a2f
VAditi18 <aditivuppugalla@gmail.com>
2024-09-21 15:29:41 +0530

    mongo_atlas

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,12 @@
 // backend/server.js
-<<<<<<< HEAD
 require("dotenv").config()
-=======
->>>>>>> e82995ae0cc200d8cb1aca76c69ac044bcd040cd
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user_routes');
 const citiesRoutes = require('./routes/city_routes');
-<<<<<<< HEAD
 const uri = process.env.DBURI
-=======
-
->>>>>>> e82995ae0cc200d8cb1aca76c69ac044bcd040cd
 
 
 const app = express();
@@ -24,12 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Database Connection
-<<<<<<< HEAD
 mongoose.connect(uri).then(() => {
-=======
-mongoose.connect('mongodb://localhost:27017/userDB', {
-}).then(() => {
->>>>>>> e82995ae0cc200d8cb1aca76c69ac044bcd040cd
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('Failed to connect to MongoDB', err);
